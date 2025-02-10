df27023051366ef8aba7849d3170676585228f0a
Saksham Bhalla <saksham.bhalla5@gmail.com>
2025-02-10 16:36:15 +0530

    updating listeners in load balancer

--- main.tf
+++ main.tf
@@ -55,13 +55,12 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  http_tcp_listeners = [
-    {
-      port = 80
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
       protocol = "HTTP"
-      target_group_index = 0
     }
-  ]
+  }
 
   target_groups = {
     ex-instance = {
