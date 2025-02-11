ad5fd02f5c9169ebb5fd5304773601334f22c751
Saksham Bhalla <saksham.bhalla5@gmail.com>
2025-02-11 13:26:24 +0530

    Adding the missing $

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "blog_vpc" {
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   
-  public_subnets  = ["{var.environment.netwrok_prefix}.101.0/24", "{var.environment.netwrok_prefix}.102.0/24", "{var.environment.netwrok_prefix}.103.0/24"]
+  public_subnets  = ["${var.environment.netwrok_prefix}.101.0/24", "${var.environment.netwrok_prefix}.102.0/24", "${var.environment.netwrok_prefix}.103.0/24"]
 
   tags = {
     Terraform = "true"
